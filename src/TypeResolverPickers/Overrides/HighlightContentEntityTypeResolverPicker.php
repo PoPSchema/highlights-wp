a561ba7786f3543ffeba4043c1ce15d59c6290a3
Leonardo Losoviz <leo@getpop.org>
2020-06-10 15:27:37 +0800

    Moved classes from PostsWP to ContentWP

--- src/TypeResolverPickers/Overrides/HighlightContentEntityTypeResolverPicker.php
+++ src/TypeResolverPickers/Overrides/HighlightContentEntityTypeResolverPicker.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\HighlightsWP\TypeResolverPickers\Overrides;
 
-use PoP\PostsWP\TypeResolverPickers\ContentEntityTypeResolverPickerInterface;
+use PoP\ContentWP\TypeResolverPickers\ContentEntityTypeResolverPickerInterface;
 use PoP\PostsWP\TypeResolverPickers\NoCastContentEntityTypeResolverPickerTrait;
 
 class HighlightContentEntityTypeResolverPicker extends \PoP\Highlights\TypeResolverPickers\Optional\HighlightContentEntityTypeResolverPicker implements ContentEntityTypeResolverPickerInterface
