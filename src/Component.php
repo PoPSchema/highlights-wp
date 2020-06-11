35efe570c27e5e898f72bf0a4d4c78e51d981c41
Leonardo Losoviz <leo@getpop.org>
2020-06-11 23:43:44 +0800

    Renamed package Content to CustomPosts

--- src/Component.php
+++ src/Component.php
@@ -20,7 +20,7 @@ class Component extends AbstractComponent
     {
         return [
             \PoP\Highlights\Component::class,
-            \PoP\ContentWP\Component::class,
+            \PoP\CustomPostsWP\Component::class,
         ];
     }
 
--- src/TypeResolverPickers/Overrides/HighlightCustomPostTypeResolverPicker.php
+++ src/TypeResolverPickers/Overrides/HighlightCustomPostTypeResolverPicker.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\HighlightsWP\TypeResolverPickers\Overrides;
 
-use PoP\ContentWP\TypeResolverPickers\CustomPostTypeResolverPickerInterface;
+use PoP\CustomPostsWP\TypeResolverPickers\CustomPostTypeResolverPickerInterface;
 use PoP\PostsWP\TypeResolverPickers\NoCastCustomPostTypeResolverPickerTrait;
 
 class HighlightCustomPostTypeResolverPicker extends \PoP\Highlights\TypeResolverPickers\Optional\HighlightCustomPostTypeResolverPicker implements CustomPostTypeResolverPickerInterface
