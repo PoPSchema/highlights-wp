cc901533ec7d313abcdb134e1cd57832cc44ccc8
Leonardo Losoviz <leo@getpop.org>
2020-06-17 14:52:34 +0800

    Removed dependency to Posts

--- src/TypeResolverPickers/Overrides/HighlightCustomPostTypeResolverPicker.php
+++ src/TypeResolverPickers/Overrides/HighlightCustomPostTypeResolverPicker.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PoP\HighlightsWP\TypeResolverPickers\Overrides;
 
 use PoP\CustomPostsWP\TypeResolverPickers\CustomPostTypeResolverPickerInterface;
-use PoP\PostsWP\TypeResolverPickers\NoCastCustomPostTypeResolverPickerTrait;
+use PoP\CustomPostsWP\TypeResolverPickers\NoCastCustomPostTypeResolverPickerTrait;
 
 class HighlightCustomPostTypeResolverPicker extends \PoP\Highlights\TypeResolverPickers\Optional\HighlightCustomPostTypeResolverPicker implements CustomPostTypeResolverPickerInterface
 {
